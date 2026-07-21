4abc5b375e6cd97fafd6d0543a7d24d318b91178
Agus Risky <240040110@stikom-bali.ac.id>
2026-07-21 20:36:45 +0700

    Menambahkan sistem penilaian

--- sistem_penilaian.py
+++ sistem_penilaian.py
@@ -42,5 +42,49 @@ def lihat_hasil():
     print ("Nilai UAS : ", niali_uas)
 
 def cek_kelulusan()
-    nilai_akhir = ...
+    nilai_akhir = (nilai_tugas * 20 / 100) + (nilai_uts * 30 / 100) + (nilai_uas * 50 / 100)
 
+    if nilai_akhir >= 85:
+    grade = "A"
+
+    elif nilai_akhir >= 75:
+        grade = "B"
+
+    elif nilai_akhir >= 65:
+        grade = "C"
+
+    elif nilai_akhir >= 50:
+        grade = "D"
+
+    else:
+        grade = "E"
+
+def reset_data():
+
+    global nama
+    global nis
+    global nilai_tugas
+    global nilai_uts
+    global nilai_uas
+    global nilai_akhir
+
+    nama = ""
+    nis = ""
+    nilai_tugas = 0
+    nilai_uts = 0
+    nilai_uas = 0
+    nilai_akhir = 0
+    
+
+    print("Data berhasil direset.")
+
+jalan = True
+
+while jalan:
+
+    menu()
+
+    pilihan = input("Masukan Pilihan : ")
+
+    if pilihan == "1":
+        input_data()
